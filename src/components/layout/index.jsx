4aa1453255dc0842e565534a5552e92107fb4060
unknown <milan_cirkovic@yahoo.com>
2020-11-18 18:31:26 +0100

    navbar created

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Navbar from "../navabar/index"
+
+import "./style.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Navbar siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <main>{children}</main>
+    </>
+  )
+}
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+export default Layout
---
+++ src/components/navabar/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import "./style.css"
+
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { FaBars, FaTimes } from "react-icons/fa"
+
+function Navbar({ siteTitle }) {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            {siteTitle}
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? (
+              <FaTimes className="fa-times" />
+            ) : (
+              <FaBars className="fa-bars" />
+            )}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+Navbar.propTypes = {
+  siteTitle: PropTypes.string,
+}
+Navbar.defaultProps = {
+  siteTitle: ``,
+}
+export default Navbar
