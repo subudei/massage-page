af72af62c2f7cd13c3eaa057ca7c741ecfb96964
unknown <milan_cirkovic@yahoo.com>
2020-11-19 22:29:24 +0100

    restyling navbar

--- src/components/navabar/index.jsx
+++ src/components/navabar/index.jsx
@@ -4,49 +4,48 @@ import "./style.css"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { FaBars, FaTimes } from "react-icons/fa"
+import { gsap } from "gsap"
 
 function Navbar({ siteTitle }) {
   const [click, setClick] = useState(false)
 
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
+  gsap.from(".active", {
+    x: "-100%",
+    duration: 0.5,
+  })
 
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            {siteTitle}
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? (
-              <FaTimes className="fa-times" />
-            ) : (
-              <FaBars className="fa-bars" />
-            )}
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/contact"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          {siteTitle}
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          {click ? (
+            <FaTimes className="menu-btn" />
+          ) : (
+            <FaBars className="menu-btn" />
+          )}
         </div>
+        <ul className={click ? "nav-menu" : "nav-menu active"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   )
